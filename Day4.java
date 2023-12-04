2fbfec264fc6a61ebc04a55e9e7f339d2550ba8c
Efrem Stefan <st.efrem98@gmail.com>
2023-12-04 13:43:09 +0200

    update with O(n) solution

--- Day4.java
+++ Day4.java
@@ -7,28 +7,17 @@ import java.nio.file.Path;
 import java.util.*;
 
 public class Day4 {
-    public static char[][] fileToCharMatrix(List<String> lines) {
-        char[][] matrix = new char[lines.size()][lines.get(0).length()];
-        for (int i = 0; i < lines.size(); i++) {
-            matrix[i] = lines.get(i).toCharArray();
-        }
-        return matrix;
-    }
 
     public static void main(String[] args) {
         Path path = Path.of("inputs/input.txt");
         try(BufferedReader bufferedReader = Files.newBufferedReader(path)){
             List<String> lines = bufferedReader.lines().toList();
-            char[][] matrix = fileToCharMatrix(lines);
             System.out.println(part1(lines)); //21821
             System.out.println(part2(lines)); //5539496
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-
-
     private static int part1(List<String> lines) {
         int result = 0;
         for(String line : lines){
@@ -63,31 +52,21 @@ public class Day4 {
     private static int part2(List<String> lines) {
         int result = 0;
         //Folosesc un HashMap pentru a pastra tuplul (nr card, numar de cards castigate de acel numar).
-        //Asa reusesc sa trec prin toate cartile castigate de la un numar
-        // Am folosit un TreeMap original, crezand ca o sa am nevoie de ordinea key-lor cand fac trecerea prin
         // scrach cards castigate dar am modificat la HashMap realizand ca asta nu este cazul
         Map<Integer, Integer> wonScratchCards = new HashMap<>();
+        // HashMap initializat la 1 deoarece avem o carte din fiecare deja, trebuie doar adunate cele castigate
+        for(int i = 0; i < lines.size(); i++){
+            wonScratchCards.put(i+1, 1);
+        }
         for(String line : lines){
             String[] cardNumberSection = line.strip().split(":")[0].split(" ");
             String cardNumber = cardNumberSection[cardNumberSection.length-1];
             int presentCardNumber = Integer.parseInt(cardNumber);
             //Functie ajutatoare pentru cautarea scratchCardurilor dintr-o linie
             p2CardParser(wonScratchCards, line);
-            // crestem rezultatul pentru cartea prezenta pe care o avem deja
-            result++;
-            int numberPresentCardsWon = wonScratchCards.getOrDefault(presentCardNumber, 0);
-            // crestem rezultatul pentru numarul de carti castigate de acest numar (castigate in trecut la cartile anterioare)
-            result += numberPresentCardsWon;
-            // aflarea rezultatului dureaza putin din cauza numarului pare de apeluri pentru cartile castigate
-            while (numberPresentCardsWon > 0){
-                p2CardParser(wonScratchCards, line);
-                numberPresentCardsWon--;
-            }
-            wonScratchCards.remove(presentCardNumber);
-
-
         }
-        return result;
+
+        return wonScratchCards.values().stream().reduce(0, Integer::sum);
     }
 
     private static void p2CardParser(Map<Integer, Integer> wonScratchCards, String line){
@@ -109,10 +88,10 @@ public class Day4 {
             if (set.contains(Integer.parseInt(number))) {
                 set.remove(0);
                 countScratchWonForCard++;
-                //cream o intrare in HashMap daca nu am mai castigat aceasta noua carte sau
-                // ii crestem numarul de carti castigate
-                wonScratchCards.merge(presentCardNumber + countScratchWonForCard, 1, Integer::sum);
             }
+        }
+        for(int i = 1 ; i <= countScratchWonForCard; i++){
+            wonScratchCards.merge(presentCardNumber + i, wonScratchCards.get(presentCardNumber), Integer::sum);
         }
     }
 }
